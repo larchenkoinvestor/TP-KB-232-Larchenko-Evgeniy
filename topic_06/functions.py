93bbc9dc77ec1f459e5b590eea0401839f7cf7aa
larchenkoinvestor <diablotop39@gmail.com>
2024-12-09 23:31:18 +0200

    add solution for topic06

--- topic_06/functions.py
+++ topic_06/functions.py
@@ -1,13 +1,24 @@
+import logging
+
 def add(a, b):
-    return a + b
+    result = a + b
+    logging.debug(f"Виконується додавання: {a} + {b} = {result}")
+    return result
 
 def subtract(a, b):
-    return a - b
+    result = a - b
+    logging.debug(f"Виконується віднімання: {a} - {b} = {result}")
+    return result
 
 def multiply(a, b):
-    return a * b
+    result = a * b
+    logging.debug(f"Виконується множення: {a} * {b} = {result}")
+    return result
 
 def divide(a, b):
     if b == 0:
+        logging.error("Спроба ділення на нуль.")
         raise ZeroDivisionError("Ділення на нуль неможливе.")
-    return a / b
+    result = a / b
+    logging.debug(f"Виконується ділення: {a} / {b} = {result}")
+    return result
--- topic_06/operations.py
+++ topic_06/operations.py
@@ -1,17 +1,5 @@
-from functions import add, subtract, multiply, divide
 import logging
-
-# Налаштування логування
-logging.basicConfig(
-    filename="calculator.log",           # Файл для збереження логів
-    level=logging.INFO,                  # Рівень логування
-    format="%(asctime)s - %(message)s",  # Формат записів
-    datefmt="%Y-%m-%d %H:%M:%S"          # Формат дати
-)
-
-def input_num(prompt):
-
-    return float(input(prompt))
+from functions import add, subtract, multiply, divide
 
 def execute_operation(command, a, b):
     operations = {
@@ -20,13 +8,13 @@ def execute_operation(command, a, b):
         "*": multiply,
         "/": divide
     }
-    if command in operations:
-        result = operations[command](a, b)
-        log_action(command, a, b, result)
-        return result
-    else:
-        raise ValueError("Невідома операція")
-
-def log_action(command, a, b, result):
-    
-    logging.info(f"Операція: {a} {command} {b} = {result}")
+    try:
+        if command in operations:
+            result = operations[command](a, b)
+            logging.debug(f"Виконано операцію {command}: {a} {command} {b} = {result}")
+            return result
+        else:
+            raise ValueError("Невідома операція")
+    except Exception as e:
+        logging.error(f"Помилка при виконанні {command}: {e}")
+        raise
